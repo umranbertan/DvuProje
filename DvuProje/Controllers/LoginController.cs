e56ce11b111a90bca228b2faf9777b65b7adefdd
umranbertan <umranbertan@gmail.com>
2021-08-31 23:10:28 +0300

    Proje dosyası ekle.

---
+++ DvuProje/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DvuProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Islem()
+        {
+            return View();
+        }
+        public ActionResult forgot()
+        {
+            return View(); // şifremi unuttum
+        }
+        
+        public ActionResult register()
+        {
+            return View(); // yeni hesap
+        }
+        public ActionResult reset()
+        {
+            return View(); // yeni hesap
+        }
+
+
+    }
+}
